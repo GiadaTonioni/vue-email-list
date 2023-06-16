b3f9cebdc020968a14771200467408f14767dea3
GiadaTonioni <giada.tonioni@gmail.com>
2023-06-16 15:43:24 +0200

    aggiunta mounted

--- js/script.js
+++ js/script.js
@@ -8,17 +8,23 @@ createApp({
 		}
 	},
     mounted(){
-        this.callRandomEmail()
+        this.callRandomEmail();
     },
     methods:{
         callRandomEmail(){
-                this.randomEmail = Math.random().toString(36).slice(2)
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
+                this.randomEmail = result.data.email
+                })
+                
             },
-            generateRandomEmailsGroup(){
-                for(let i=0; i<= 10; i++){
-                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
-                    this.emailList.push(result.data.response)
-                    })
+        createRandomEmail(){
+            this.randomEmail = Math.random().toString(36).slice(2)
+        },
+        generateRandomEmailsGroup(){
+            for(let i=0; i<= 10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
+                this.emailList.push(result.data.response)
+                })
                 }
             }
         }
