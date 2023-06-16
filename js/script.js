64a268c48fb121f12682fd9cc509ed5bc49f4285
GiadaTonioni <giada.tonioni@gmail.com>
2023-06-16 15:27:01 +0200

    prove

--- js/script.js
+++ js/script.js
@@ -3,30 +3,25 @@ const {createApp} = Vue;
 createApp({
 	data(){
 		return{
-            email: '',
-            emailList: 
-            [
-                'uno@gmail.com', 
-                'due@gmail.com', 
-                'tre@gmail.com', 
-                'quattro@gmail.com', 
-                'cinque@gmail.com', 
-                'sei@gmail.com',
-                'sette@gmail.com',
-                'otto@gmail.com',
-                'nove@gmail.com',
-                'dieci@gmail.com'
-            ]
+            randomEmail: '',
+            emailList: []
 		}
 	},
-	
+    mounted(){
+        this.callRandomEmail()
+    },
     methods:{
-        callEmail(){
-                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
-                    this.emailList = result.data.response
-                })
-                
+        callRandomEmail(){
+                this.randomEmail = Math.random().toString(36).slice(2)
+            },
+            generateRandomEmailsGroup(){
+                for(let i=0; i<= 10; i++){
+                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
+                    this.emailList.push(result.data.response)
+                    })
+                }
             }
         }
     }).mount('#app')
+                
             
